5f089faea25acc283964612c9e1d900183ccb79e
Tien Dung Le <95424716+tienthedev@users.noreply.github.com>
2024-05-17 17:28:11 -0400

    added 2 new elements

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,23 +1,21 @@
 'use client';
 import React from 'react';
-import {JetBrains_Mono, League_Spartan} from "next/font/google";
+import {League_Spartan} from "next/font/google";
 
-const jetbrains_mono =  JetBrains_Mono({subsets: ['latin']})
 const league_spartan = League_Spartan({subsets: ['latin']})
-
+//
 const About: React.FC = () => {
     return (
         <div id={"hero"}
              className={`h-screen w-screen flex flex-col justify-center items-center text-left -tracking-wide px-10 lg:px-56 ${league_spartan.className} z-0`}>
             <div className="flex flex-col">
-                <h1 className={`lg:text-lg text-md text-white font-thin animate-fadeUp delay-0 hidden-initially ${jetbrains_mono.className}`}>Hi, my name is</h1>
-                <h1 className={`lg:text-9xl text-7xl font-extrabold tracking-tighter text-secondary animate-fadeUp delay-200 hidden-initially `}>Tien
-                    Le.</h1>
-                <h1 className={`lg:text-7xl text-5xl font-bold tracking-tighter text-tertiary animate-fadeUp delay-400 hidden-initially`}>I
-                    research, and develop computer vision and machine learning models.</h1>
-                <h1 className="lg:text-2xl text-md text-white  animate-fadeUp delay-600 hidden-initially">
-                    {"I'm a software developer with a background in computer science. I often research about UI/UX, as well as Artificial Intelligent methods to implement to my projects."}
-                </h1>
+
+                <p className="lg:text-2xl text-md text-tertiary  animate-fadeUp delay-600 hidden-initially">
+                    During my academic career, I've seen many significant effects and usages of <span className="text-white"> Artificial Intelligent </span>in our daily lives.
+                    This observation led me to focus in the <span className="text-white"> research and development of AI, Machine Learning, and Deep Learning,</span> here at the <span className="text-white"> University of Texas in Arlington,</span> where I am currently pursing a <span className="text-white"> Bachelor of Science in Computer Science. </span>
+                    As of now, I am a third-year student, with two technology internship experiences in both
+                    <span className="text-white"> automotive, aviation, airlines, and finance.</span>
+                </p>
             </div>
         </div>
     );
