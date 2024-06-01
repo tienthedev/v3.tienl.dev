b0db91fe7a60a216da972b117e7493efe61cd03d
Tien Dung Le <95424716+tienthedev@users.noreply.github.com>
2024-05-31 22:24:56 -0500

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ const league_spartan = League_Spartan({subsets: ['latin']})
 const About: React.FC = () => {
     return (
         <div id={"about"}
-             className={`h-screen w-screen flex flex-col justify-center items-center text-left -tracking-wide px-10 lg:px-56 ${league_spartan.className} z-0`}>
+             className={`h-screen  flex flex-col justify-center items-center text-left -tracking-wide px-10 lg:px-56 ${league_spartan.className} z-0`}>
             <div className="flex flex-col">
 
                 <p className="lg:text-2xl text-md text-tertiary  animate-fadeUp delay-600 hidden-initially">
